9d76b2cfc18bb8407d8247f07fcd626df1b1cec0
KylinWu <midnightkylin@gmail.com>
2018-05-11 22:28:52 +0800

    Implement MTProto sendMedia

--- MTProto/index.js
+++ MTProto/index.js
@@ -1,11 +1,12 @@
 const login = require('./login');
-const { getForeignPeer, sendMsg } = require('./sendMsg');
+const { getForeignPeer, sendMsg, sendMedia } = require('./sendMsg');
 
 function MTProto () {
 }
 
 MTProto.prototype.login = login;
 MTProto.prototype.sendMsg = sendMsg;
+MTProto.prototype.sendMedia = sendMedia;
 MTProto.prototype.getForeignPeer = getForeignPeer;
 
 module.exports = MTProto;
--- MTProto/sendMsg.js
+++ MTProto/sendMsg.js
@@ -1,4 +1,7 @@
-const telegram = require('./init')
+const telegram = require('./init');
+const fs = require('mz/fs');
+const crypto = require('crypto');
+const path = require('path');
 
 async function getForeignPeer(username) {
     
@@ -19,6 +22,61 @@ async function getForeignPeer(username) {
     .catch(console.log);
 }
 
+async function sendMedia(peer, file) {
+    const _512k = 512 * 1024;
+    let bytes;
+    let md5_hash;
+    await fs.readFile(file).then(data => {
+        bytes = data;
+    })
+    .catch(console.log);
+    md5_hash = await crypto.createHash('md5').update(bytes).digest('hex');
+    const fileId = Math.random() * 10e7 >> 0;
+    const parts = Math.ceil(bytes.length/_512k);
+    var start = 0, end;
+    for (var i=0; i<parts; i++) {
+        end = start + _512k;
+        if (end > bytes.length) {
+            end = bytes.length;
+        }
+        await telegram('upload.saveFilePart',  {
+            file_id: fileId,
+            file_part: i,
+            bytes: bytes.slice(start, end)
+        })
+        .then()
+        .catch(console.log);
+        start = end;
+    }
+
+    await telegram('messages.sendMedia', {
+        peer: {
+            _: 'inputPeerUser',
+            user_id: peer.user_id,
+            access_hash: peer.access_hash
+        },
+        media: {
+            _: 'inputMediaUploadedDocument',
+            flags: 0,
+            file: {
+                _: 'inputFile',
+                id: fileId,
+                parts: parts,
+                name: file,
+                md5_checksum: md5_hash
+            },
+            mime_type: 'image/png',
+            attributes: [{
+                _: 'documentAttributeFilename',
+                file_name: path.basename(file, '.png')
+            }],
+        },
+        random_id: Math.random() * 10e7 >> 0
+    })
+    .then(console.log('Send stick : ' + path.basename(file)))
+    .catch(console.log);
+}
+
 async function sendMsg(peer, message) {
     await telegram('messages.sendMessage', {
         peer: {
@@ -29,8 +87,8 @@ async function sendMsg(peer, message) {
         message: message,
         random_id: Math.random() * 10e7 >> 0
     })
-    .then(console.log)
+    .then(console.log('Send Message :' + message))
     .catch(console.log);
 }
 
-module.exports = { sendMsg, getForeignPeer };
+module.exports = { sendMsg, sendMedia, getForeignPeer };
