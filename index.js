9863ea56c13cdf66914c1e25cc6f91a9ebc70631
KylinWu <midnightkylin@gmail.com>
2018-05-21 16:31:22 +0800

    Implement operating flow
    Use TDL API

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 require('dotenv').config({path: __dirname + '/.env'});
-const TelegramBot = require('node-telegram-bot-api');
-const MTProto = require('./MTProto');
 const API = require('./API');
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
-const mtproto = new MTProto();
+const TDL = require('./TDL');
+const TelegramBot = require('node-telegram-bot-api');
 const api = new API();
+const tdl = new TDL();
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
 
 const fs = require('mz/fs');
 const path = require('path');
 const emoji = require('node-emoji');
 
 const userChatId = process.env.TELEGRAM_USER_CHAT_ID;
+
+const timeout = ms => new Promise(res => setTimeout(res, ms))
+const delay = async  ms => {
+      await timeout(ms)
+}
 
 const Stage = {
     "Idle": 0,
@@ -26,10 +31,9 @@ const helpMsg =
     `/newpack StickerPackID - create a new sticker pack\n` + 
     `/cancel - cancel the current operation`;
 
-async function run() {
-    //await mtproto.login();
-    await mtproto.auth();
-    const peer = await mtproto.getForeignPeer('@Stickers');
+const run = async () => {
+    await tdl.auth();
+    const stickersChatId = await tdl.getChatId('@Stickers');
     bot.onText(/\/start/, async (msg, match) => {
         const chatId = msg.chat.id;
         if (chatId == userChatId) {
@@ -84,39 +88,33 @@ async function run() {
                     bot.sendMessage(chatId, 'Wait a second, still downloading Line sticker pack.');
                     break;
                 case Stage.WaitPackName:
-                    await mtproto.sendMsg(peer, '/cancel');
-                    await mtproto.sendMsg(peer, '/newpack');
-                    await mtproto.sendMsg(peer, input);
+                    await tdl.sendMsg(stickersChatId, '/cancel');
+                    await tdl.sendMsg(stickersChatId, '/newpack');
+                    await tdl.sendMsg(stickersChatId, input);
                     await bot.sendMessage(chatId, 'Alright! Now we are uploading sticker, Please wait a minute.');
                     currentStage = Stage.Uploading;
+
                     await fs.readdir('./StickerSets/pack')
                     .then(async (stickers) => {
                         const total = stickers.length;
-                        var fileList = new Array();
                         let count = 1;
+                        let match, lastMsg;
+                        const regex = /Thanks! Now send me an emoji that corresponds to your first sticker\./g;
                         for (let sticker of stickers) {
                             const stickerPath = path.resolve('./StickerSets/pack/', path.basename(sticker));
-                            console.log('Sticker file part uploading... ' + count++ + '/' + total);
-                            const filePartData = await mtproto.sendFilePart(peer, stickerPath);
-                            fileList.push(filePartData);
-                        }
-                        count = 1;
-                        const regex = /Thanks! Now send me an emoji that corresponds to your first sticker\./g;
-                        let match, lastMsg;
-                        for (let list of fileList) {
-                            console.log('Sending sitcker... ' + count + '/' + total);
-                            await bot.sendMessage(chatId, 'Uploading...(' + count++ + '/' + total + ')');
-                            mtproto.sendMedia(peer, list.file, list.fileId);
+                            console.log('Sending stickers... ' + count + '/' + total);
+                            await tdl.sendFile(stickersChatId, stickerPath);
                             do {
-                                console.log('Waiting lastMsg');
-                                lastMsg = await mtproto.getLastMsg(peer);
+                                await delay(800);
+                                lastMsg = await tdl.getLastMsg(stickersChatId);
                                 match = regex.test(lastMsg);
                                 console.log((match)?'Response Checked.':'Not yet response.');
                             } while (match !== true);
-                            await mtproto.sendMsg(peer, emoji.emojify(':small_blue_diamond:'));
+                            await tdl.sendMsg(stickersChatId, emoji.emojify(':small_blue_diamond:'));
+                            await bot.sendMessage(chatId, 'Uploading...(' + count++ + '/' + total + ')');
                         }
                         await bot.sendMessage(chatId, 'Upload done!');
-                        await mtproto.sendMsg(peer, '/publish');
+                        await tdl.sendMsg(stickersChatId, '/publish');
                         await bot.sendMessage(chatId, 'Please provide a short name for your sticker pack.');
                         currentStage = Stage.WaitShortName;
                     })
@@ -127,9 +125,10 @@ async function run() {
                     break;
                 case Stage.WaitShortName:
                     const regex = /Sorry, this short name is already taken\./g;
-                    mtproto.sendMsg(peer, input);
+                    tdl.sendMsg(stickersChatId, input);
                     do {
-                        lastMsg = await mtproto.getLastMsg(peer);
+                        await delay(800);
+                        lastMsg = await tdl.getLastMsg(stickersChatId);
                     } while (!lastMsg)
                     if (regex.test(lastMsg)) {
                         bot.sendMessage(chatId, 'Sorry, this short name is already taken. Give me another one.');
