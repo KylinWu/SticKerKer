9f6a65202c8e12b991f49d8cebc48305ad200246
KylinWu <midnightkylin@gmail.com>
2018-05-22 16:47:33 +0800

    Start stickers bot before sendMessage

--- TDL/query.js
+++ TDL/query.js
@@ -7,10 +7,28 @@ const searchPublicChat = async username => {
     return await telegram.invoke({
         _: 'searchPublicChat',
         username: username
-    })
+    });
+}
+
+const sendBotStartMessage = async bot_user_id => { 
+    await telegram.invoke({
+        _: 'sendBotStartMessage',
+        bot_user_id: bot_user_id,
+        chat_id: bot_user_id,
+        parameter: 'start'
+    });
+}
+
+const getUserId = async username => {
+    return await searchPublicChat(username)
+    .then(result => {
+        return result.id;
+    });
 }
 
 const getChatId = async username => {
+    const bot_user_id = await getUserId(username);
+    await sendBotStartMessage(bot_user_id);
     return await searchPublicChat(username)
     .then(result => {
         return result.last_message.chat_id;
